9af57721930272f1db8aa58ab8954dfac8ab5049
RegisLewis <regisaslewis@gmail.com>
2023-11-21 18:38:03 -0800

    Problems

--- server/models.py
+++ server/models.py
@@ -4,3 +4,29 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    hairstyle = db.Column(db.String)
+
+    # products_owned = db.relationship("Product", back_populates="user")
+
+    def __repr__(self):
+        return f"User# {self.id}: {self.username}"
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    condition = db.Column(db.Integer)
+    is_new = db.Column(db.Boolean, default=False, nullable=False)
+
+    # owners = db.relationship("User", back_populates="products")
+
+    def __repr__(self):
+        return f"Product# {self.id}: {self.name}"
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,26 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Product
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+
         print("Starting seed...")
         # Seed code goes here!
+        ted = User(username="Ted", age=45, hairstyle="wavy, long")
+        marla = User(username="Marla", age=33, hairstyle="straight, mid-length")
+        frannie = User(username="Frannie", age=24, hairstyle="curly, short")
+        users = [ted, marla, frannie]
+
+        red_rover = Product(name="Red Rover", condition=3, is_new=False)
+        new_unbreakable = Product(name="Unbreakable", condition=5, is_new=True)
+        old_reliable = Product(name="Old Reliable", condition=1, is_new=False)
+        products = [red_rover, new_unbreakable, old_reliable]
+
+        db.session.add_all(users)
+        db.session.add_all(products)
+
+        db.session.commit()
+        print("Seeding Complete.")

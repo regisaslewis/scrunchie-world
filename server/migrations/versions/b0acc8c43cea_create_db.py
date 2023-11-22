a956daf89aa007b2bcc6a6f63db1d9537ed97533
RegisLewis <regisaslewis@gmail.com>
2023-11-21 18:38:11 -0800

    Problems

---
+++ server/migrations/versions/b0acc8c43cea_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: b0acc8c43cea
+Revises: 
+Create Date: 2023-11-21 18:27:02.790803
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b0acc8c43cea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

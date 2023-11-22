6c7b16aa584dadf6adc133b00dd62eebc892cdf9
RegisLewis <regisaslewis@gmail.com>
2023-11-21 19:47:27 -0800

    set up basic models

---
+++ server/migrations/versions/dc8435d4eb5d_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: dc8435d4eb5d
+Revises: 
+Create Date: 2023-11-21 19:18:32.093532
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc8435d4eb5d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

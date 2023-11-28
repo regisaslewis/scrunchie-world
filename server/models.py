21c2e8810adda4b5468d924f1d70757ef7989256
RegisLewis <regisaslewis@gmail.com>
2023-11-28 15:06:40 -0800

    added all models

--- server/models.py
+++ server/models.py
@@ -13,8 +13,6 @@ class User(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
     hairstyle = db.Column(db.String)
 
-    # products_owned = db.relationship("Product", back_populates="user")
-
     def __repr__(self):
         return f"User# {self.id}: {self.username}"
 
@@ -22,11 +20,38 @@ class Product(db.Model, SerializerMixin):
     __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, unique=True)
     condition = db.Column(db.Integer)
     is_new = db.Column(db.Boolean, default=False, nullable=False)
 
-    # owners = db.relationship("User", back_populates="products")
-
     def __repr__(self):
         return f"Product# {self.id}: {self.name}"
+    
+class Group(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String)
+
+    members = []
+
+    def __repr__(self):
+        return f"Group #{self.id}: {self.name} | {('Members: ', self.members) if self.members else 'No current members'}"
+    
+class Brand(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String)
+
+    products = []
+
+    def __repr__(self):
+        return f"Brand #{self.id}: {self.name}{(' | Products: ', self.products) if self.products else ''}"
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment = db.Column(db.String)
--- server/seed.py
+++ server/seed.py
@@ -8,24 +8,47 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User, Product
+from models import db, User, Product, Review, Group, Brand
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
 
+        print("Deleting old info...")
+        Review.query.delete()
+        Group.query.delete()
+        Brand.query.delete()
+        User.query.delete()
+        Product.query.delete()
+
         print("Starting seed...")
         # Seed code goes here!
+        all_business = Group(name="All Business", description="Function is everything.")
+        chic_cheeky = Group(name="Chic and Cheeky", description="Stylish but playful.")
+        go_squad = Group(name="The Go Squad", description="On the move.")
+        fans = Group(name="Just Admiring", description="Afficiandos from afar.")
+        groups = [all_business, chic_cheeky, go_squad, fans]
+
+        palace = Brand(name="The Palace", description="Elegance in form.")
+        retro = Brand(name="Vintagity", description="Remember when...")
+        astro = Brand(name="Zodiac", description="Guided by the stars.")
+        active = Brand(name="EnMotion", description="Stopping is death.")
+        brands =[palace, retro, astro, active]
+
         ted = User(username="Ted", age=45, hairstyle="wavy, long")
         marla = User(username="Marla", age=33, hairstyle="straight, mid-length")
         frannie = User(username="Frannie", age=24, hairstyle="curly, short")
-        users = [ted, marla, frannie]
+        debra = User(username="Debra", age=35, hairstyle="bald")
+        users = [ted, marla, frannie, debra]
 
         red_rover = Product(name="Red Rover", condition=3, is_new=False)
         new_unbreakable = Product(name="Unbreakable", condition=5, is_new=True)
         old_reliable = Product(name="Old Reliable", condition=1, is_new=False)
-        products = [red_rover, new_unbreakable, old_reliable]
+        gorrilla_grip = Product(name="Gorrilla Grip", condition=4, is_new=False)
+        products = [red_rover, new_unbreakable, old_reliable, gorrilla_grip]
 
+        db.session.add_all(groups)
+        db.session.add_all(brands)
         db.session.add_all(users)
         db.session.add_all(products)
 

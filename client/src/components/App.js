0092a75822462f8d0498f1a3d0aeb8dc69a2bd01
RegisLewis <regisaslewis@gmail.com>
2023-11-29 18:53:57 -0800

    added routes to client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import { NavBar } from NavBar;
+import { Brand } from Brand;
+import { Group } from Group;
+import { SignUp } from SignUp;
 
 function App() {
-  return <h1>Scrunchie World Client</h1>;
+  return (
+    <>
+      <NavBar/>
+      <h1>Scrunchie World Client</h1>;
+      <SignUp/>
+      <Brand/>
+      <Group/>
+    </>
+  )
 }
 
 export default App;
